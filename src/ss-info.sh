699801816affa08c7b9a3177c592dbbcfa243694
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-10-11 15:52:22 +0800

    번역

--- src/ss-info.sh
+++ src/ss-info.sh
@@ -2,20 +2,20 @@
 if [[ $shadowsocks ]]; then
 	local ss="ss://$(echo -n "${ssciphers}:${sspass}@${ip}:${ssport}" | base64 -w 0)#233v2.com_ss_${ip}"
 	echo
-	echo "---------- Shadowsocks 配置信息 -------------"
+	echo "---------- Shadowsocks 설정 정보 -------------"
 	echo
-	echo -e "$yellow 服务器地址 = $cyan${ip}$none"
+	echo -e "$yellow 서버 주소 = $cyan${ip}$none"
 	echo
-	echo -e "$yellow 服务器端口 = $cyan$ssport$none"
+	echo -e "$yellow 서버 포트 = $cyan$ssport$none"
 	echo
-	echo -e "$yellow 密码 = $cyan$sspass$none"
+	echo -e "$yellow 암호 = $cyan$sspass$none"
 	echo
-	echo -e "$yellow 加密协议 = $cyan${ssciphers}$none"
+	echo -e "$yellow 암호화 프로토콜 = $cyan${ssciphers}$none"
 	echo
-	echo -e "$yellow SS 链接 = ${cyan}$ss$none"
+	echo -e "$yellow SS 링크 = ${cyan}$ss$none"
 	echo
-	echo -e " 备注:$red Shadowsocks Win 4.0.6 $none客户端可能无法识别该 SS 链接"
+	echo -e " 참고:$red Shadowsocks Win 4.0.6 $none클라이언트는 SS 링크를 식별하지 못합니다."
 	echo
-	echo -e "提示: 输入$cyan v2ray ssqr $none可生成 Shadowsocks 二维码链接"	
+	echo -e " 참고: $cyan v2ray ssqr $none 명령어로 Shadowsocks QR코드를 생성할 수 있습니다."	
 	echo
 fi
