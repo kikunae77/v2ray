b16a6e4765ca692410a553d075db8cf51b266c5d
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-10-11 12:29:00 +0800

    번역

--- src/bt.sh
+++ src/bt.sh
@@ -1,19 +1,19 @@
 _ban_bt_main() {
 	if [[ $ban_bt ]]; then
-		local _info="$green已开启$none"
+		local _info="$green켬$none"
 	else
-		local _info="$red已关闭$none"
+		local _info="$red끔$none"
 	fi
 	_opt=''
 	while :; do
 		echo
-		echo -e "$yellow 1. $none开启 BT 屏蔽"
+		echo -e "$yellow 1. $noneBT 차단 켜기"
 		echo
-		echo -e "$yellow 2. $none关闭 BT 屏蔽"
+		echo -e "$yellow 2. $noneBT 차단 끄기"
 		echo
-		echo -e "当前 BT 屏蔽状态: $_info"
+		echo -e "기존 BT 차단 상태: $_info"
 		echo
-		read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" _opt
+		read -p "$(echo -e "선택해주세요 [${magenta}1-2$none]:")" _opt
 		if [[ -z $_opt ]]; then
 			error
 		else
@@ -21,12 +21,12 @@ _ban_bt_main() {
 			1)
 				if [[ $ban_bt ]]; then
 					echo
-					echo -e " 大胸弟...难不成你没有看到 (当前 BT 屏蔽状态: $_info) 这个帅帅的提示么.....还开启个鸡鸡哦"
+					echo -e " 기존 BT 차단 상태: $_info) 입니다.....다시 켤 필요 없습니다."
 					echo
 				else
 					echo
 					echo
-					echo -e "$yellow  BT 屏蔽 = $cyan开启$none"
+					echo -e "$yellow  BT 차단 = $cyan켬$none"
 					echo "----------------------------------------------------------------"
 					echo
 					pause
@@ -35,7 +35,7 @@ _ban_bt_main() {
 					config
 					echo
 					echo
-					echo -e "$green  BT 屏蔽已开启...如果出现异常..那就关闭它咯$none"
+					echo -e "$green  BT 차단이 시작되었습니다...에러가 발생하면, 이 기능을 꺼주세요.$none"
 					echo
 				fi
 				break
@@ -44,7 +44,7 @@ _ban_bt_main() {
 				if [[ $ban_bt ]]; then
 					echo
 					echo
-					echo -e "$yellow  BT 屏蔽 = $cyan关闭$none"
+					echo -e "$yellow  BT 차단 = $cyan끔$none"
 					echo "----------------------------------------------------------------"
 					echo
 					pause
@@ -53,11 +53,11 @@ _ban_bt_main() {
 					config
 					echo
 					echo
-					echo -e "$red  BT 屏蔽已关闭...不过你也可以随时重新开启 ...只要你喜欢$none"
+					echo -e "$red  BT 차단이 꺼졌습니다...원하시면 다시 켜실 수 있습니다.$none"
 					echo
 				else
 					echo
-					echo -e " 大胸弟...难不成你没有看到 (当前 BT 屏蔽状态: $_info) 这个帅帅的提示么.....还关闭个鸡鸡哦"
+					echo -e " 기존 BT 차단 상태: $_info) 입니다.....다시 끌 필요 없습니다."
 					echo
 				fi
 				break
