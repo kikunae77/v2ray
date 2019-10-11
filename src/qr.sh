74840fafce1b9e4aea4795defb30c6c8b691002a
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-10-11 12:53:31 +0800

    번역

--- src/qr.sh
+++ src/qr.sh
@@ -2,15 +2,15 @@ _qr_create() {
 	local vmess="vmess://$(cat /etc/v2ray/vmess_qr.json | base64 -w 0)"
 	local link="https://233boy.github.io/tools/qr.html#${vmess}"
 	echo
-	echo "---------- V2Ray 二维码链接 适用于 V2RayNG v0.4.1+ / Kitsunebi -------------"
+	echo "---------- V2Ray QR코드 링크 V2RayNG v0.4.1+ / Kitsunebi 이상에 사용 가능 -------------"
 	echo
 	echo -e ${cyan}$link${none}
 	echo
 	echo
-	echo -e "$red 友情提醒: 请务必核对扫码结果 (V2RayNG 除外) $none"
+	echo -e "$red 스캔 결과를 꼭 확인하세요 (V2RayNG 이외) $none"
 	echo
 	echo
-	echo " V2Ray 客户端使用教程: https://233v2.com/post/4/"
+	echo " V2Ray 클라이언트 사용 강좌(중국어): https://233v2.com/post/4/"
 	echo
 	echo
 	rm -rf /etc/v2ray/vmess_qr.json
@@ -19,11 +19,11 @@ _ss_qr() {
 	local ss_link="ss://$(echo -n "${ssciphers}:${sspass}@${ip}:${ssport}" | base64 -w 0)#233v2.com_ss_${ip}"
 	local link="https://233boy.github.io/tools/qr.html#${ss_link}"
 	echo
-	echo "---------- Shadowsocks 二维码链接 -------------"
+	echo "---------- Shadowsocks QR코드 링크 -------------"
 	echo
-	echo -e "$yellow 链接 = $cyan$link$none"
+	echo -e "$yellow 링크 = $cyan$link$none"
 	echo
-	echo -e " 温馨提示...$red Shadowsocks Win 4.0.6 $none客户端可能无法识别该二维码"
+	echo -e " 주의...$red Shadowsocks Win 4.0.6 $none클라이언트는 QR코드 인식이 불가능합니다."
 	echo
 	echo
 }
