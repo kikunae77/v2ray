397ee0bd056388dcb3f45b2d1f36e6efe3b2fe77
kikunae77 <54462749+kikunae77@users.noreply.github.com>
2019-10-11 12:30:47 +0800

    번역

--- src/client_file.sh
+++ src/client_file.sh
@@ -4,11 +4,11 @@ _get_client_file() {
     local _link="$(cat $v2ray_client_config | tr -d [:space:] | base64 -w0)"
     local link="https://233boy.github.io/tools/json.html#${_link}"
     echo
-    echo "---------- V2Ray 客户端配置文件链接 -------------"
+    echo "---------- V2Ray 클라이언트 설정 정보 링크 -------------"
     echo
     echo -e ${cyan}$link${none}
     echo
-    echo " V2Ray 客户端使用教程: https://233v2.com/post/4/"
+    echo " V2Ray 클라이언트 사용 강좌(중국어): https://233v2.com/post/4/"
     echo
     echo
 }
